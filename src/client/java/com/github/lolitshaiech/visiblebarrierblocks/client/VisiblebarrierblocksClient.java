b1b5646e17dab6fd985055ee9c0c53ec40d11de9
lolitshaiech <seyedamin13845@gmail.com>
2024-09-07 03:04:27 +0330

    poggor

--- src/client/java/com/github/lolitshaiech/visiblebarrierblocks/client/VisiblebarrierblocksClient.java
+++ src/client/java/com/github/lolitshaiech/visiblebarrierblocks/client/VisiblebarrierblocksClient.java
@@ -4,11 +4,9 @@ import com.github.lolitshaiech.visiblebarrierblocks.client.init.KeyInputInit;
 import net.fabricmc.api.ClientModInitializer;
 
 public class VisiblebarrierblocksClient implements ClientModInitializer {
-
     public static final String MOD_ID = "visiblebarrierblocks";
     public static final String MOD_NAME = "Visible Barrier Blocks";
 
-    public static final boolean visible = true;
 
     @Override
     public void onInitializeClient() {
