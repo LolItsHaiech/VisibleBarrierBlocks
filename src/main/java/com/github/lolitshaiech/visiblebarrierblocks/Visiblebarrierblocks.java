3ba6d8a041eac7152b5d8f87a115c36fca94c71c
lolitshaiech <seyedamin13845@gmail.com>
2024-09-07 02:10:57 +0330

    poggor

---
+++ src/main/java/com/github/lolitshaiech/visiblebarrierblocks/Visiblebarrierblocks.java
@@ -0,0 +1,10 @@
+package com.github.lolitshaiech.visiblebarrierblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Visiblebarrierblocks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
